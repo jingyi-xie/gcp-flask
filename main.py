5b3f327249159a9121dac56f19990e436e2d0032
Jingyi Xie <jx95@duke.edu>
2021-02-02 04:38:13 +0000

    added html

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World! I am Jingyi'
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
